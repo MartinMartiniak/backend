bc186ecd4cb8f776432fae2976bea9fb00cb709f
Martin Martiniak <martiniak.martin15@gmail.com>
2021-10-26 09:50:03 +0200

    Create a student class which asks for their name and number

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,26 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        Student student1 = new Student("John", 12345);
+        System.out.println(student1.getName() + ": " + student1.getStudentNumber());
+    }
+}
+
+class Student{
+    private String Name;
+    private int StudentNumber;
+
+    public Student(String name, int studentNumber){
+        Name = name;
+        StudentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getStudentNumber() {
+        return StudentNumber;
     }
 }
