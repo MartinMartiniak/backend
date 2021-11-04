131293bd0b173f5ccc4a40737e1e0d0d1a99d156
z19ista18 <martiniak.martin15@gmail.com>
2021-11-04 09:17:39 +0100

    Get area and perimeter out of the current values.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,10 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args){
-        Triangle triangle1 = new Triangle(3,4,5)
-    }
+        Triangle triangle1 = new Triangle(3,4,5);
+
+
+        System.out.println("Area:"+ Triangle.getArea(triangle1.getSidea(),triangle1.getSideb(),triangle1.getSidec()));
+        System.out.println("Perimeter:"+Triangle.getPerimeter(triangle1.getSidea(),triangle1.getSideb(),triangle1.getSidec()));
+    };
 }
--- src/com/appslab/Triangle.java
+++ src/com/appslab/Triangle.java
@@ -22,4 +22,14 @@ public class Triangle {
     public int getSidec() {
         return Sidec;
     }
+
+    public static int getArea(int Sidea, int Sideb, int Sidec)
+    {
+        return Sidea+Sidec/2;
+    }
+    public static int getPerimeter(int Sidea, int Sideb, int Sidec)
+    {
+        return Sidea+Sideb+Sidec;
+
+    }
 }
