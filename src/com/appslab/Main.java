38c27062676bc79f7da466b149541b82b4130dd0
z19ista18 <martiniak.martin15@gmail.com>
2021-11-04 08:43:51 +0100

    Triangle class and preparations for main calling

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,7 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args){
+        Triangle triangle1 = new Triangle(3,4,5)
     }
 }
---
+++ src/com/appslab/Triangle.java
@@ -0,0 +1,25 @@
+package com.appslab;
+
+public class Triangle {
+    public int Sidea;
+    public int Sideb;
+    public int Sidec;
+
+    public Triangle (int sidea, int sideb, int sidec){
+        Sidea = sidea;
+        Sideb = sideb;
+        Sidec = sidec;
+    }
+
+    public int getSidea() {
+        return Sidea;
+    }
+
+    public int getSideb() {
+        return Sideb;
+    }
+
+    public int getSidec() {
+        return Sidec;
+    }
+}
